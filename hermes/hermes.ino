5e7b1b509d0490d85cd7fb7c8b1d1711e5a32533
Reed Morse <reed.morse@gmail.com>
2013-07-13 00:21:24 -0700

    proof of concept for accel

--- hermes/hermes.ino
+++ hermes/hermes.ino
@@ -1,24 +1,68 @@
+// LED imports
 #include "LPD8806.h"
 #include "SPI.h"
+
+// Accel imports
+#include "Adafruit_LSM303.h"
+#include "Wire.h"
 
 int LED_COUNT = 16;
 int DATA_PIN = 9;
 int CLOCK_PIN = 10;
 int COLOR_RANGE = 384;
 
+void setup() {
+  //colorSetup();
+  accelSetup();
+  Serial.begin(9600);
+}
+
+void loop() {
+  //showColors();
+  pollAccel(50);
+}
+
+
+///////////
+// accel //
+///////////
+
+Adafruit_LSM303 lsm;
+
+void accelSetup() {
+  lsm.begin();
+}
+
+void pollAccel(int delayMilliseconds) {
+  lsm.read();
+  int x = lsm.accelData.x;
+  int y = lsm.accelData.y;
+  int z = lsm.accelData.z;
+  
+  Serial.print(x); Serial.print (", ");
+  Serial.print(y); Serial.print (", ");
+  Serial.print(z);
+  Serial.println();
+  
+  delay(delayMilliseconds);
+}
+
+
+///////////
+// color //
+///////////
+
 LPD8806 strip = LPD8806(LED_COUNT, DATA_PIN, CLOCK_PIN);
 
-void setup() {
+void colorSetup() {
   // Turn the strip on.
   strip.begin();
   
   // Refresh the strip.
   strip.show();
-  
-  Serial.begin(9600);
 }
 
-void loop() {
+void showColors() {
   for (int j = 0; j < 384; j++) {
     for (int i = 0; i < strip.numPixels(); i++) {
       strip.setPixelColor(i, color(j, 0.5));
